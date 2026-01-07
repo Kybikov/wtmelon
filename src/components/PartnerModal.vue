ee8466f55505425044be84fd2c4b6b4de9991ad8
Whoami_WT <142737772+Kybikov@users.noreply.github.com>
2026-01-07 15:41:04 +0100

    Updated style.css

---
+++ src/components/PartnerModal.vue
@@ -0,0 +1,84 @@
+<template>
+  <Transition name="modal">
+    <div v-if="modelValue" class="modal-overlay" @click="closeModal">
+      <div class="modal-container partner-modal" @click.stop>
+        <button class="modal-close" @click="closeModal">
+          <i class="fas fa-times"></i>
+        </button>
+
+        <div class="modal-header">
+          <div class="partner-logo-large">
+            <img :src="partner.logo" :alt="partner.name">
+          </div>
+          <div>
+            <h2>{{ t(`partners.${partner.id}.name`) }}</h2>
+          </div>
+        </div>
+
+        <div class="modal-body">
+          <div class="partner-description">
+            <p>{{ t(`partners.${partner.id}.description`) }}</p>
+          </div>
+
+          <div class="partnership-section">
+            <h4><i class="fas fa-handshake"></i> Наше партнерство</h4>
+            <p>{{ t(`partners.${partner.id}.partnership`) }}</p>
+          </div>
+
+          <div class="benefits-section">
+            <h4><i class="fas fa-star"></i> Переваги</h4>
+            <ul class="benefits-list">
+              <li v-for="(benefit, index) in t(`partners.${partner.id}.benefits`)" :key="index">
+                <i class="fas fa-check-circle"></i>
+                <span>{{ benefit }}</span>
+              </li>
+            </ul>
+          </div>
+
+          <div class="modal-footer">
+            <a
+              :href="t(`partners.${partner.id}.website`)"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="btn1 visit-btn"
+            >
+              <i class="fas fa-external-link-alt"></i>
+              {{ t('partners.visitWebsite') }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+import { useLocale } from '../composables/useLocale'
+
+export default {
+  name: 'PartnerModal',
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true
+    },
+    partner: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const { t } = useLocale()
+
+    const closeModal = () => {
+      emit('update:modelValue', false)
+    }
+
+    return {
+      t,
+      closeModal
+    }
+  }
+}
+</script>
--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -6,31 +6,71 @@
         <p class="subtitle">{{ t('partners.subtitle') }}</p>
       </div>
       <div class="partners-grid">
-        <div class="partner-card">
+        <div
+          v-for="partner in partners"
+          :key="partner.id"
+          class="partner-card"
+          @click="openPartnerModal(partner)"
+        >
           <div class="partner-logo">
-            <img src="https://4cd6555acz.ucarecd.net/9bd83bd9-1ef4-4e7f-9c40-53e0f6fcd99a/rozetka.svg" alt="Rozetka">
+            <img :src="partner.logo" :alt="partner.name">
           </div>
-          <h3>Rozetka</h3>
-        </div>
-        <div class="partner-card">
-          <div class="partner-logo">
-            <img src="https://4cd6555acz.ucarecd.net/75766c92-72a1-4d8e-aad4-dd8e07bef87e/sweettv.svg" alt="Sweet TV">
-          </div>
-          <h3>Sweet TV</h3>
+          <h3>{{ partner.name }}</h3>
+          <button class="btn-partner-details">
+            {{ t('partners.learnMore') }}
+            <i class="fas fa-arrow-right"></i>
+          </button>
         </div>
       </div>
     </div>
+
+    <PartnerModal
+      v-model="showModal"
+      :partner="selectedPartner"
+    />
   </section>
 </template>
 
 <script>
+import { ref } from 'vue'
 import { useLocale } from '../composables/useLocale'
+import PartnerModal from './PartnerModal.vue'
 
 export default {
   name: 'Partners',
+  components: {
+    PartnerModal
+  },
   setup() {
     const { t } = useLocale()
-    return { t }
+    const showModal = ref(false)
+    const selectedPartner = ref({})
+
+    const partners = [
+      {
+        id: 'rozetka',
+        name: 'Rozetka',
+        logo: 'https://4cd6555acz.ucarecd.net/9bd83bd9-1ef4-4e7f-9c40-53e0f6fcd99a/rozetka.svg'
+      },
+      {
+        id: 'sweettv',
+        name: 'Sweet TV',
+        logo: 'https://4cd6555acz.ucarecd.net/75766c92-72a1-4d8e-aad4-dd8e07bef87e/sweettv.svg'
+      }
+    ]
+
+    const openPartnerModal = (partner) => {
+      selectedPartner.value = partner
+      showModal.value = true
+    }
+
+    return {
+      t,
+      partners,
+      showModal,
+      selectedPartner,
+      openPartnerModal
+    }
   }
 }
 </script>
